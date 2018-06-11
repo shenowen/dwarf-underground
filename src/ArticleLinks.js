0befdb2ae2baeb571a4f919ea871c0a086318f24
Owen Shen <shenowen36@gmail.com>
2018-06-11 16:07:09 -0400

    Completed Basic Hw, with one Component in another(Article>Links)

---
+++ src/ArticleLinks.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class ArticleLinks extends Component{
+    render(){
+        return(
+            <div className="article-links">
+            <a className="article-link" href="#">
+              <i className="fa fa-comments-o"></i>
+              <span className="article-link-text">Comments Test</span>
+            </a>
+            <a className="article-link" href="#">
+              <i className="fa fa-share"></i>
+              <span className="article-link-text">Share Post</span>
+            </a>
+          </div>
+        )
+    }
+}
+
+export default ArticleLinks
